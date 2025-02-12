b25d468f937c3cdf7515af84fa798c6b6e2d3b34
Ayokunle Amodu <121697771+ayokunle321@users.noreply.github.com>
2025-02-11 17:30:53 -0700

    Add bfloat16 type

---
+++ libc/include/llvm-libc-types/bfloat16.h
@@ -0,0 +1,16 @@
+//===-- Definition of bfloat16 type ---------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIBC_TYPES_BFLOAT16_H
+#define LLVM_LIBC_TYPES_BFLOAT16_H
+
+// TODO: Set up compiler/platform conditions for type definition
+#define LIBC_TYPES_HAS_BFLOAT16
+typedef bfloat16;
+
+#endif // LLVM_LIBC_TYPES_CFLOAT16_H
